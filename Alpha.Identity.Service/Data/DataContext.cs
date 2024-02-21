6f207fc86ab4c73bb7c6053f3b167d849cebe9fb
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-02-21 10:31:58 -0300

    Add Microsoft Identity

---
+++ Alpha.Identity.Service/Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alpha.Identity.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext(options)
+{
+}
--- Alpha.Identity.Service/Program.cs
+++ Alpha.Identity.Service/Program.cs
@@ -1,25 +1,60 @@
+using Alpha.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace Alpha.Identity;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Run(Build(args));
+    }
+
+    private static WebApplication Build(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();        
+        builder.Services.AddControllers();
         builder.Services.AddHealthChecks();
         builder.Services.AddSwaggerGen();
 
+        builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")!));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddAuthorization();
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+           .AddEntityFrameworkStores<DataContext>();
 
         var app = builder.Build();
+        return app;
+    }
 
+    private static void Run(WebApplication app)
+    {
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.MapIdentityApi<IdentityUser>();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
         app.MapControllers();
         app.MapHealthChecks("/health");
 
+        MigrateDb(app);
+
         app.Run();
+    }
+
+
+    private static void MigrateDb(WebApplication app)
+    {
+        var scope = app.Services.CreateScope();
+        var dc = scope.ServiceProvider.GetRequiredService<DataContext>();
+        dc.Database.Migrate();
     }
 }
