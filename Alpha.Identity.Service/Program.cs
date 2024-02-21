cddfe189d0a4862bd183d20d06119e28971a86e7
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-02-21 09:42:28 -0300

    Add Health check

--- Alpha.Identity.Service/Program.cs
+++ Alpha.Identity.Service/Program.cs
@@ -1,12 +1,16 @@
+namespace Aplha.Identity;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();        
+        builder.Services.AddHealthChecks();
 
         var app = builder.Build();
         app.MapControllers();
+        app.MapHealthChecks("/health");
 
         app.Run();
     }
